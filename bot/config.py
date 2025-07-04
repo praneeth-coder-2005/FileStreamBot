0b2b4fc46622456838ce5a4d8b156f48a9595a9c
praneeth-coder-2005 <savediskofc@gmail.com>
2025-07-04 14:44:45 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -11,9 +11,9 @@ class Telegram:
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 24))
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "http://127.0.0.1:8089")
+    BASE_URL = env.get("BASE_URL", "http://127.0.0.1:8080")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
-    PORT = int(env.get("PORT", 8089))
+    PORT = int(env.get("PORT", 8080))
 
 # LOGGING CONFIGURATION
 LOGGER_CONFIG_JSON = {
